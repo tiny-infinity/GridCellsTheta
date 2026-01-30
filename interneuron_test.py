4c5a958e5a006746d412c2867e72fd6f6f7290a9
tiny-infinity <govind.rnair@students.iiserpune.ac.in>
2026-01-30 17:25:51 +0530

    theta modulatin + phase locking

--- interneuron_test.py
+++ interneuron_test.py
@@ -3,6 +3,7 @@ from neuron import h
 import interneuron_amb
 import matplotlib.pyplot as plt
 import numpy as np
+import analysis_utils as a_utils
 importlib.reload(interneuron_amb)
 
 h.load_file("stdrun.hoc")
@@ -12,9 +13,9 @@ test_intrnrn = interneuron_amb.Interneuron(1)
 theta_ic = h.IClamp(test_intrnrn.soma(0.5))
 theta_ic.dur = 1e9
 freq = 10
-time_ms = 1000
-osc_amp = 10e-4
-baseline_amp = 0.8e-3
+time_ms = 5000
+osc_amp = 5e-4
+baseline_amp = 1e-3
 T, num_steps = time_ms/1000,time_ms
 time_arr = np.linspace(0,T,num_steps)
 curr_arr = osc_amp * np.sin(2 * np.pi * freq * time_arr) + np.full_like(time_arr, baseline_amp)
@@ -34,9 +35,19 @@ test_intrnrn_v = h.Vector().record(test_intrnrn.soma(0.5)._ref_v)
 time = h.Vector().record(h._ref_t)
 
 h.finitialize(-65)
-h.continuerun(1000)
+h.continuerun(5000)
 fig,ax = plt.subplots(figsize=(10,6))
 ax1 = ax.twinx()
 ax.plot(time, i_theta.to_python(), color='gray', linestyle='--', label='Theta Input')
 ax1.plot(time, test_intrnrn_v, label='Interneuron')
+plt.show()
+
+spike_times_intrnrn_np = np.array(spike_times_intrnrn.to_python())
+
+spike_rate = a_utils.instant_rate(spike_times_intrnrn_np,5000,50)
+
+fig,ax = plt.subplots(figsize=(10,6))
+ax2 = ax.twinx()
+ax.plot(time_arr, spike_rate, color='red', label='Spike Rate')
+ax2.plot(np.array(time)/1000, i_theta.to_python(), color='gray', linestyle='--', label='Theta Input')
 plt.show()
